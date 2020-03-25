392e1903c01fac51a618768e779612a976ad26f9
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-03-25 15:30:29 +0100

    add pinephone-specific feedbackd theme

--- tweaks/pinephone-tweaks.sh
+++ tweaks/pinephone-tweaks.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+# Fix Qt applications
 export QT_QPA_PLATFORM=wayland
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+# Fix Firefox
 export MOZ_ENABLE_WAYLAND=1
+
+# Use custom feedbackd theme
+export FEEDBACK_THEME=/usr/share/feedbackd/themes/pinephone.json
