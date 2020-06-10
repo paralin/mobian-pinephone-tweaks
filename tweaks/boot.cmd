4ec9afb22903178eb2f6f0f816ecfe65c5681ed0
Arnaud Ferraris <arnaud.ferraris@gmail.com>
2020-06-10 02:02:46 +0200

    boot.cmd: allow booting from a separate /boot partition

--- tweaks/boot.cmd
+++ tweaks/boot.cmd
@@ -1,27 +1,35 @@
-if test ${mmc_bootdev} -eq 0 ; then
-	echo "Booting from SD";
-	setenv linux_mmcdev 0;
+if test ${mmc_bootdev} -eq 0; then
+	echo "Booting from SD"
+	setenv linux_mmcdev 0
 else
-	echo "Booting from eMMC";
-	setenv linux_mmcdev 2;
-fi;
+	echo "Booting from eMMC"
+	setenv linux_mmcdev 2
+fi
 
-setenv bootargs console=ttyS0,115200 no_console_suspend panic=10 consoleblank=0 loglevel=7 root=/dev/mmcblk${linux_mmcdev}p1 ro splash plymouth.ignore-serial-consoles vt.global_cursor_default=0
+# If we have at least 2 partitions, then the 1st one is /boot, and / is #2
+if fstype mmc ${mmc_bootdev}:2; then
+	setenv rootpart 2
+else
+	setenv rootpart 1
+	setenv bootdir "/boot"
+fi
+
+setenv bootargs console=ttyS0,115200 no_console_suspend panic=10 consoleblank=0 loglevel=7 root=/dev/mmcblk${linux_mmcdev}p${rootpart} ro splash plymouth.ignore-serial-consoles vt.global_cursor_default=0
 
 led 1 on
 
 echo "Loading kernel..."
-load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} /boot/Image.gz
+load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} ${bootdir}/Image.gz
 
 echo "Uncompressing kernel..."
 unzip ${ramdisk_addr_r} ${kernel_addr_r}
 
 echo "Loading initramfs..."
-load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} /boot/initrd.img
+load mmc ${mmc_bootdev}:1 ${ramdisk_addr_r} ${bootdir}/initrd.img
 setenv ramdisk_size ${filesize}
 
 echo "Loading dtb..."
-load mmc ${mmc_bootdev}:1 ${fdt_addr_r} /boot/dtb/${fdtfile}
+load mmc ${mmc_bootdev}:1 ${fdt_addr_r} ${bootdir}/dtb/${fdtfile}
 
 led 2 on
 
