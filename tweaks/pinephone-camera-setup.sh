94227415a2f5f5ab6fe695fa63b50364d7414259
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-07-06 15:12:26 +0200

    pinephone-camera-setup: use back camera by default

--- tweaks/pinephone-camera-setup.sh
+++ tweaks/pinephone-camera-setup.sh
@@ -2,3 +2,8 @@
 
 # Configure camera
 media-ctl -d /dev/media0 --set-v4l2 '"ov5640 3-004c":0[fmt:YUYV8_2X8/1280x720]'
+media-ctl -d /dev/media0 --set-v4l2 '"gc2145 3-003c":0[fmt:YUYV8_2X8/1280x720]'
+
+# Select back camera
+media-ctl -d /dev/media0 --links '"gc2145 3-003c":0->"sun6i-csi":0[0]'
+media-ctl -d /dev/media0 --links '"ov5640 3-004c":0->"sun6i-csi":0[1]'
