23b25860650b75c3792a7aa1cb97fe2be4126efb
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-07-14 17:43:28 +0200

    tweaks: add suspend hooks
    This allows use to do a few different things:
    - enable power save mode for the modem
    - disable bluetooth to avoid a kernel OOPS
    - restore system time on resume

---
+++ tweaks/pinephone-suspend-hook.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+prepare_suspend() {
+	# Put modem in power saving mode
+	echo -e 'AT+QSCLK=1\r' > /dev/ttyUSB2
+	echo 1 > /sys/class/gpio/gpio358/value
+
+	# Disable Bluetooth module (fixes kernel OOPS)
+	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/unbind
+}
+
+resume_all() {
+	# Wake up modem
+	echo 0 > /sys/class/gpio/gpio358/value
+	echo -e 'AT+QSCLK=0\r' > /dev/ttyUSB2
+
+	# Restore system time from RTC
+	hwclock -s
+
+	# Re-enable Bluetooth module
+	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/bind
+}
+
+case $1 in
+	pre) prepare_suspend ;;
+	post) resume_all ;;
+esac
