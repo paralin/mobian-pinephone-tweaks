bcfe3cec149453c66d3e8e04a0ecffabd95d6427
Arnaud Ferraris <arnaud.ferraris@gmail.com>
2020-07-20 03:13:40 +0200

    pinephone-suspend-hook: toggle AP_READY when going in/out of suspend

--- tweaks/pinephone-suspend-hook.sh
+++ tweaks/pinephone-suspend-hook.sh
@@ -2,23 +2,29 @@
 
 prepare_suspend() {
 	# Put modem in power saving mode
-	echo -ne 'AT+QSCLK=1\r' > /dev/ttyS2
+	# Note: GPIO231 is WAKEUP_IN on BH and AP_READY on CE
+	#  - BH: WAKEUP_IN must be high to enable power saving mode
+	#  - CE: AP_READY (active low) must be high to indicate host sleep
+	# In both cases DTR (GPIO358) must be high to enable power saving mode
+	echo 1 > /sys/class/gpio/gpio231/value
 	echo 1 > /sys/class/gpio/gpio358/value
+	echo -ne 'AT+QSCLK=1\r' > /dev/ttyS2
 
 	# Disable Bluetooth module (fixes kernel OOPS)
 	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/unbind
 }
 
 resume_all() {
-	# Wake up modem
-	echo 0 > /sys/class/gpio/gpio358/value
-	echo -ne 'AT+QSCLK=0\r' > /dev/ttyS2
-
 	# Restore system time from RTC
 	hwclock -s
 
 	# Re-enable Bluetooth module
 	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/bind
+
+	# Wake up modem
+	echo -ne 'AT+QSCLK=0\r' > /dev/ttyS2
+	echo 0 > /sys/class/gpio/gpio231/value
+	echo 0 > /sys/class/gpio/gpio358/value
 }
 
 case $1 in
