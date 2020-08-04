dad7db770aa341d0658c883073d7a6a74f31a8e5
Arnaud Ferraris <arnaud.ferraris@gmail.com>
2020-08-04 15:55:48 +0200

    pinephone-suspend-hooks: enable URC caching during suspend

--- tweaks/pinephone-suspend-hook.sh
+++ tweaks/pinephone-suspend-hook.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 prepare_suspend() {
+	# Enable URC caching
+	echo -ne 'AT+QCFG="urc/cache",1\r' > /dev/ttyS2
+
 	# Put modem in power saving mode
 	# Note: GPIO231 is WAKEUP_IN on BH and AP_READY on CE
 	#  - BH: WAKEUP_IN must be high to enable power saving mode
@@ -16,6 +19,9 @@ resume_all() {
 	echo -ne 'AT+QSCLK=0\r' > /dev/ttyS2
 	echo 0 > /sys/class/gpio/gpio231/value
 	echo 0 > /sys/class/gpio/gpio358/value
+
+	# Disable URC caching
+	echo -ne 'AT+QCFG="urc/cache",0\r' > /dev/ttyS2
 }
 
 case $1 in
